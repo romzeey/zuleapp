6572e461d46c4a148b4752a1197f4dcaa7ba9832
romzeey <chimezule.uchendu@gmail.com>
2020-07-31 22:58:18 +0100

    first commit

---
+++ zuleapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ZuleappConfig(AppConfig):
+    name = 'zuleapp'
---
+++ zuleapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'index.html')
+
+def about_us(request):
+    return render(request, 'about_us.html')
+
+def contact_us(request):
+    return render(request, 'contact_us.html')
